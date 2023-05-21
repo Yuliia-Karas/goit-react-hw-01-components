b4f042170e69ccaa7d07a7dc72a08cc9a4e02b6a
Yuliia Karas <iuliiakaras@gmail.com>
2023-05-21 09:20:22 +0200

    Profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,36 @@
+import './Profile.css'
+
+
+export const Profile = (props) => {
+    const { username, tag, location, avatar, stats } = props
+    return (
+        <div className="profile">
+  <div class="description">
+    <img
+      src={avatar}
+      alt="User avatar"
+      class="avatar"
+    />
+    <p class="name">{username}</p>
+    <p class="tag">@p{tag}</p>
+    <p class="location">{location}</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+    )
+}
+ 

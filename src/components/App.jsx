4044c3e06b8d374848e921ab1805ed884cd4d74e
Yuliia Karas <iuliiakaras@gmail.com>
2023-05-23 18:57:24 +0200

    prettier handmade

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,18 +22,18 @@ export const App = () => {
       //   color: '#010101'
       // }}
     >
-      <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+    <Statistics title="Upload stats" stats={data} />
             
-      <FriendList friends={friends} />;
+    <FriendList friends={friends} />;
 
-      <TransactionHistory items={transactions} />;
+    <TransactionHistory items={transactions} />;
     </div>
   );
 };
